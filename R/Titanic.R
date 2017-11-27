4b9bdd1ea3072bf1452bdc900dddfef740833f17
venkatkasarla <venkat_kasarla@yahoo.com>
2017-11-26 18:17:00 -0600

    Titanic.R ggplot

--- R/Titanic.R
+++ R/Titanic.R
@@ -3,19 +3,16 @@
 str(titanic)
 
 # 2 - Use ggplot() for the first instruction
-<<<<<<< HEAD
+
 ggplot(titanic, aes(x =  factor(Pclass), fill = factor(Sex))) +
   geom_bar(position = "dodge")
 
 # 3 - Plot 2, add facet_grid() layer
 ggplot(titanic, aes(x =  factor(Pclass), fill = factor(Sex))) +
-=======
-ggplot(titanic, aes(x = Pclass, fill = Sex)) +
   geom_bar(position = "dodge")
 
 # 3 - Plot 2, add facet_grid() layer
 ggplot(titanic, aes(x = Pclass, fill = Sex)) +
->>>>>>> f4b4994606b41848e6b12a3c79553936dd63163b
   geom_bar(position = "dodge") +
   facet_grid( . ~ Survived)
 
@@ -23,11 +20,8 @@ ggplot(titanic, aes(x = Pclass, fill = Sex)) +
 posn.jd <- position_jitterdodge(0.5, 0, 0.6)
 
 # 5 - Plot 3, but use the position object from instruction 4
-<<<<<<< HEAD
+
 ggplot(titanic, aes(x =  factor(Pclass), y =Age, col = factor(Sex))) +
-=======
-ggplot(titanic, aes(x = Pclass, y =Age, col = Sex)) +
->>>>>>> f4b4994606b41848e6b12a3c79553936dd63163b
   geom_point(size = 3, alpha=0.5,position = posn.jd ) +
   facet_grid( . ~ Survived)
 
